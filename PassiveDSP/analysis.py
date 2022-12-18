d79454ba2e665ce6b401709b8039390c13c61238
DatBoi-BP@github.com <benjamin.penazek@gmail.com>
2022-12-18 15:00:09 -0500

    First commit, initializing

---
+++ PassiveDSP/analysis.py
@@ -0,0 +1,16 @@
+# This file includes the mathematical tools necessary to analysis digital signals.
+
+if __name__ == "__main__":
+    print('Hello.')
+
+import numpy as np
+
+
+def fft(data: np.ndarray, symmetric=True):
+    data_fft = data.copy()
+    data_fft = np.fft.fft(data_fft)
+    if symmetric:
+        data_fft = np.fft.fftshift(data_fft)
+    return data_fft
+
+
